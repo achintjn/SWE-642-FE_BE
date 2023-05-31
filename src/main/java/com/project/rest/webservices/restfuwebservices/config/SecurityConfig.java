bdafe90dedb238e2135f32e023525aa5866c39ac
achintjn <achintjain66@gmail.com>
2023-05-31 18:15:04 -0500

    config java change

--- src/main/java/com/project/rest/webservices/restfuwebservices/config/SecurityConfig.java
+++ src/main/java/com/project/rest/webservices/restfuwebservices/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.project.rest.webservices.restfuwebservices.config;
+ package com.project.rest.webservices.restfuwebservices.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
